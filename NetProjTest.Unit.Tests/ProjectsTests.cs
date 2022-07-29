59e2800c5d76f2a329d77337c3ace64531bed3da
Marcin Torba <torbamarcin@gmail.com>
2022-07-30 00:44:37 +0200

    Moved samples from class to file.

--- NetProjTest.Unit.Tests/ProjectsTests.cs
+++ NetProjTest.Unit.Tests/ProjectsTests.cs
@@ -2,34 +2,22 @@ namespace NetProjTest.Unit.Tests;
 
 public class ProjectsTests
 {
-    private const string SampleProjectNameFirst = "sampleProjectNet60First";
-    private const string SampleProjectNameWithExtenstionFirst = $"{SampleProjectNameFirst}.csproj";
-    private const string SampleProjectPathFirst = $"{SampleProjectNameWithExtenstionFirst}";
-    
-    private const string SampleProjectNameSecond = "sampleProjectNet60Second";
-    private const string SampleProjectNameWithExtenstionSecond = $"{SampleProjectNameSecond}.csproj";
-    private const string SampleProjectPathSecond = $"{SampleProjectNameWithExtenstionSecond}";
-    
-    public ProjectsTests()
-    {
-        File.WriteAllText(SampleProjectPathFirst, ProjectSamples.testProjectNet60_Simple);
-        File.WriteAllText(SampleProjectPathSecond, ProjectSamples.testProjectNet60_Simple);
-    }
-    
+    private static string SearchPath => $"{Environment.CurrentDirectory}//TestSamples";
+        
     [Fact]
     public void SearchFiles_Should_ReturnAllFiles()
     {
-        var allFiles = IProjects.SearchFiles(Environment.CurrentDirectory, "*.csproj", SearchOption.TopDirectoryOnly).ToArray();
+        var allFiles = IProjects.SearchFiles(SearchPath, "*.csproj", SearchOption.TopDirectoryOnly).ToArray();
         
         allFiles.Length.Should().Be(2);
-        allFiles.Should().Contain(x => x.Contains(SampleProjectNameWithExtenstionFirst));
-        allFiles.Should().Contain(x => x.Contains(SampleProjectNameWithExtenstionSecond));
+        allFiles.Should().Contain(x => x.Contains("Net60WithPackageAndFile"));
+        allFiles.Should().Contain(x => x.Contains("Net60Simple"));
     }
     
     [Fact]
     public void SearchFiles_Should_ReturnAsIEnumerable()
     {
-        var allFiles = IProjects.SearchFiles(Environment.CurrentDirectory, "*.csproj", SearchOption.TopDirectoryOnly);
+        var allFiles = IProjects.SearchFiles(SearchPath, "*.csproj", SearchOption.TopDirectoryOnly);
 
         allFiles.Should().BeAssignableTo<IEnumerable<string>>();
     }
@@ -37,12 +25,12 @@ public class ProjectsTests
     [Fact]
     public void FromFiles_Should_ReturnValidData()
     {
-        var allFiles = IProjects.SearchFiles(Environment.CurrentDirectory, "*.csproj", SearchOption.TopDirectoryOnly)
+        var allFiles = IProjects.SearchFiles(SearchPath, "*.csproj", SearchOption.AllDirectories)
             .ToArray();
 
         if (IProjects.FromFiles(allFiles) is not ProjectsTester projects)
             throw new InvalidCastException($"Returned type must be of type {typeof(ProjectsTester)}");
 
-        projects.Projects.Count().Should().Be(2);
+        projects.Projects.Count().Should().Be(3);
     }
 }
